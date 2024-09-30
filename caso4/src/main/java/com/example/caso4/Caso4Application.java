25182a43d07b1d5615747c6e1b76cf3f60e11b96
Emmanuel MJ <85372632+empTBn@users.noreply.github.com>
2024-09-30 11:05:11 -0600

    spring initializr

---
+++ caso4/src/main/java/com/example/caso4/Caso4Application.java
@@ -0,0 +1,13 @@
+package com.example.caso4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caso4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caso4Application.class, args);
+	}
+
+}
---
+++ caso4/src/test/java/com/example/caso4/Caso4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caso4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Caso4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
